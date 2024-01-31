7e8b4cb9c1cff2bb741dddaa12d3fc7ef2e7061d
Cristian Chiera <cristianchiera@gmail.com>
2024-01-31 13:35:58 -0300

    nav y headers

--- functions.js
+++ functions.js
@@ -1,17 +1,47 @@
+const nav= `      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">
+    <img src="img/escudo.png" alt="MINSEG-MZA" width="40" height="46">
+  </a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sujetos.html">Registro</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="actividad.html">Actividades</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="operaciones.html">Operaciones</a>
+  </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          Dropdown
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </li>
 
-const relacion= document.getElementById('relacion');
-const tipoPersona = document.getElementById('tipoPersona');
-const valorTipoPersona= tipoPersona.value 
-switch (valorTipoPersona) {
-    case '1':
-        let op = document.createElement('op');
-        op.setAttribute('value', '1');
-        op.innerHTML = 'Dueño / Propietario';
-        relacion.appendChild(op);
-    break;
-    case '2':
-        document.getElementById('empresa').style.display = 'block';
-        break;
-    default:
-        document.getElementById('persona').style.display = 'block';
-}
+  </div>
+</div>
+</nav`;
+
+export {nav};
